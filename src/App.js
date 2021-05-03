f112b562f10fde4f1aaa4563d5e07a12ff49adef
Al <alexkhien@hotmail.com>
2021-05-03 10:29:03 -0400

    Separated header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './styles/styles.css';
+import Header from './components/header/header';
 
 const App = () => {
   return (
     <div>
-      <h1>Hello.</h1>
-      <h2>I'm Alexandre Khien, a Canadian Computer Science student. You can check out my projects here.</h2>
+      <Header />
     </div>
   )
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import '../../styles/headerStyle.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hello.</h1>
+            <h2>I'm Alexandre Khien, a Canadian Computer Science student. You can check out my projects here.</h2>
+        </div>
+    )
+}
+
+export default Header;
