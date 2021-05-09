b20de38224f63c03372177f746662898e2781bfc
Al <alexkhien@hotmail.com>
2021-05-08 20:55:00 -0400

    git push

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <div>
             <h1>Hello.</h1>
             <h2>I'm Alexandre Khien, a Canadian Computer Science student. You can check out my projects here!</h2>
+            <h3>Need more ideas</h3>
         </div>
     )
 }
